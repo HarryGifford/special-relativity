9af479e8aba3e8250eb442b37d2773cc7a1ac664
HarryGifford <HarryGifford@users.noreply.github.com>
2021-01-03 05:26:04 +0000

    deploy: 80371bd8a6c9220fba25fbc5401a037b74b388ab

--- camera.d.ts
+++ camera.d.ts
@@ -2,6 +2,8 @@ import { Scene, Vector3, TargetCamera } from "@babylonjs/core";
 export interface RelativisticCamera extends TargetCamera {
     velocity: Vector3;
     properVelocity: Vector3;
+    /** Set the max (coordinate) speed. */
+    setMaxSpeed(speed: number | undefined): void;
 }
 export declare const createCamera: (name: string, position: Vector3, scene: Scene) => RelativisticCamera;
 //# sourceMappingURL=camera.d.ts.map
